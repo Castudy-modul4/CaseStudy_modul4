e5a5537da0c0eb499e2cb6faf0f8c0d157727bbd
PhuocHoangg <164506464+PhuocHoangg@users.noreply.github.com>
2025-03-17 20:32:16 +0700

    sieu update

--- CaseStudyModule4_QuanLyCanHoChungCu/src/main/java/org/example/casestudymodule4_quanlycanhochungcu/repository/ICanHoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.casestudymodule4_quanlycanhochungcu.repository;
-
-import org.example.casestudymodule4_quanlycanhochungcu.entity.CanHo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICanHoRepository extends JpaRepository<CanHo, Integer> {
-}
--- CaseStudyModule4_QuanLyCanHoChungCu/src/main/java/org/example/casestudymodule4_quanlycanhochungcu/service/ITinhTrangCoSoVatChatService.java
+++
@@ -1,8 +0,0 @@
-package org.example.casestudymodule4_quanlycanhochungcu.service;
-
-import org.example.casestudymodule4_quanlycanhochungcu.entity.TinhTrangCoSoVatChat;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ITinhTrangCoSoVatChatService extends IServiceTin<TinhTrangCoSoVatChat>{
-}
